e0e8ebbad7d9e648d1bc0b4ae846b6034abfd7eb
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-03-30 13:03:35 +0300

    update posts count and return recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def update_posts_counter
+    user.update(posts_count: user.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
