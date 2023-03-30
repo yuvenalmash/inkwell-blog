92ebd26c358e2aac81fe645035f780e96b7d9224
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-03-30 16:54:56 +0300

    update associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: :author_id, class_name: 'Post'
+  has_many :comments, foreign_key: :author_id, class_name: 'Comment'
+  has_many :likes, foreign_key: :author_id, class_name: 'Like'
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
