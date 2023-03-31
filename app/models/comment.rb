febdb9fe2523539f586299101af974454a596613
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-03-31 13:27:53 +0300

    update associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  validates :text, presence: true
 
   after_create :update_comments_counter
 
