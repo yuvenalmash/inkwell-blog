2d13b34eb3fd6d1927059862d7242ee7e39e7fcf
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-08 01:06:07 +0300

    Add comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :posts do
       resources :likes, only: %i[create destroy]
+      resources :comments, only: %i[create destroy]
     end
   end
 end
