a383a78d820d89714a934ca34a5a3813edc987f7
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-07 17:31:01 +0300

    Add new_post functionality for current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,27 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
   end
 
+  def new
+    @post = @user.posts.new
+  end
+
+  def create
+    @post = @user.posts.new(post_params)
+
+    if @post.save
+      redirect_to [@user, @post]
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
