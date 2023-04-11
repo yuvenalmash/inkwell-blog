7652b06c8c68bef3fba16dac8c690e2f2f62b8ac
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-11 11:57:23 +0300

    fix linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,7 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.find_or_initialize_by(post: @post)
-    if @like.persisted?
-      @like.destroy
-      notice = "You unliked this post"
-    elsif @like.save
-      notice = "You liked this post"
-    else
-      notice = "Could not like"
-    end
+    @like.persisted? ? @like.destroy : @like.save
     head :no_content
   end
 
@@ -18,9 +11,8 @@ class LikesController < ApplicationController
     @like = current_user.likes.find_by(post: @post)
     if @like
       @like.destroy
-      flash[:notice] = "You unliked this post"
     else
-      flash[:alert] = "Could not unlike"
+      flash[:alert] = 'Could not unlike'
     end
     head :no_content
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: "user_id"
-  belongs_to :post, class_name: "Post", foreign_key: "post_id"
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   after_create :update_likes_counter
   after_destroy :update_likes_counter
