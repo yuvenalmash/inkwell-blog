0a5973a22441842324897753c757d0ca5e204fb4
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-12 22:47:25 +0300

    Add confirmable attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :registerable,
          :recoverable,
          :rememberable,
-         :validatable
+         :validatable,
+         :confirmable
   has_many :posts, foreign_key: :user_id, class_name: 'Post'
   has_many :comments, foreign_key: :user_id, class_name: 'Comment'
   has_many :likes,
