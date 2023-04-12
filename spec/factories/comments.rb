73b86c0d438a3e54b4a9540c777860cb6116bfab
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-12 04:01:35 +0300

    Add see comments test in post index view

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -3,11 +3,7 @@ require 'faker'
 FactoryBot.define do
   factory :comment do
     text { Faker::Lorem.paragraph }
-    trait :post_id do
-      association :post
-    end
-    trait :author do
-      association :user
-    end
+    association :author, factory: :user
+    association :post, factory: :post
   end
 end
--- spec/views/posts/show_spec.rb
+++ spec/views/posts/show_spec.rb
@@ -2,40 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'show posts page', type: :feature do
   before do
-    @user = FactoryBot.create(:user)
-    @post1 = FactoryBot.create(:post, author: @user)
-    @post2 = FactoryBot.create(:post, author: @user)
-    @post3 = FactoryBot.create(:post, author: @user)
-    @post4 = FactoryBot.create(:post, author: @user)
-    @post5 = FactoryBot.create(:post, author: @user)
-    @post6 = FactoryBot.create(:post, author: @user)
-    @comment1 = FactoryBot.create(:comment, post: @post6, author: @user)
-    @comment2 = FactoryBot.create(:comment, post: @post6, author: @user)
-    @comment3 = FactoryBot.create(:comment, post: @post6, author: @user)
-    visit user_post_path(@user, @post1)
+    @user1 = FactoryBot.create(:user)
+    @user2 = FactoryBot.create(:user)
+    @post1 = FactoryBot.create(:post, author: @user1)
+    @comment1 = FactoryBot.create(:comment, author: @user2, post: @post1)
+    visit user_post_path(@user1, @post1)
   end
 
-  scenario 'show page url' do
-    expect(page).to have_current_path(user_post_path(@user, @post1))
+  scenario "I can see the post's title" do
+    expect(page).to have_content(@post1.title)
   end
 
-  scenario 'show page has post title' do
-    assert page.has_content?(@post1.title)
+  scenario 'I can see who wrote the post' do
+    expect(page).to have_content(@user1.name)
   end
 
-  scenario 'show page has a posts author\'s name' do
-    assert page.has_content?(@user.name)
+  scenario 'I can see how many comments it has' do
+    expect(page).to have_content('Comments: 1')
   end
 
-  scenario 'show page has no of comments and likes on a post' do
-    expect(page).to have_content('Comments: 0 Likes: 0')
+  scenario 'I can see how many likes it has' do
+    expect(page).to have_content('Likes: 0')
   end
 
-  scenario 'show page has a posts body' do
-    assert page.has_selector?('p', text: @post1.text)
+  scenario 'I can see the post body' do
+    expect(page).to have_content(@post1.text)
   end
 
-  scenario 'show page has comment\'s username' do
-    assert page.has_selector?('p', text: @comment1.author.name)
+  scenario 'I can see the username of each commentor' do
+    expect(page).to have_content(@user2.name)
+  end
+
+  scenario 'I can see the comment each commentor left' do
+    expect(page).to have_content(@comment1.text)
   end
 end
