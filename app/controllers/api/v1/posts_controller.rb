35baa91f51d8c9dd2eafd58f3033e3f13926a990
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-14 19:48:15 +0300

    Add skip before action

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_post, only: [:show]
   before_action :set_user
 
   def index
